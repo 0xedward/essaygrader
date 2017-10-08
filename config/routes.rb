9cd0bb4ceb1f823bac4fdf24ad94e3e2500db6a1
Edward <14011954+0xedward@users.noreply.github.com>
2017-10-07 21:08:57 -0700

    finished file upload and restricting to common doc types

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+    resources :documents, only: [:index, :new, :create, :destroy]
+    root "documents#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
